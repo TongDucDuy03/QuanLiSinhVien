1341b841b72df86656a3e2019872cca61a05aee2
Toan <truongtoan00123@gmail.com>
2023-10-26 00:29:34 +0700

    db

--- test3/Models/Model1.Context.cs
+++ test3/Models/Model1.Context.cs
@@ -32,6 +32,5 @@ namespace test3.Models
         public virtual DbSet<Lop> Lops { get; set; }
         public virtual DbSet<MonHoc> MonHocs { get; set; }
         public virtual DbSet<SinhVien> SinhViens { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
     }
 }
