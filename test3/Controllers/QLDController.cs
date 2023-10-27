74d1c2d475cb52e0b8d503e27e966040eab75a64
Tống Đức Duy <tongducduy4@gmail.com>
2023-10-27 22:16:00 +0700

    update

---
+++ test3/Controllers/QLDController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test3.Controllers
+{
+    public class QLDController : Controller
+    {
+        // GET: QLD
+        public ActionResult DanhSachDiem()
+        {
+            return View();
+        }
+    }
+}
