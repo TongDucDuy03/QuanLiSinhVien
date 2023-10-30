6d611effa86179900fd8ae28b414d022d039a0f8
bntien <tien0206166@nuce.edu.vn>
2023-10-31 00:30:53 +0700

    Update QLGVController.cs

--- test3/Controllers/QLGVController.cs
+++ test3/Controllers/QLGVController.cs
@@ -104,23 +104,70 @@ namespace test3.Controllers
         {
             QLSVEntities db = new QLSVEntities();
             var giangVien = db.GiangViens.Find(id);
-            if (giangVien.GioiTinh)
-            {
-                ViewBag.NamChecked = true;
-                ViewBag.NuChecked = false;
-            }
-            else
-            {
-                ViewBag.NamChecked = false;
-                ViewBag.NuChecked = true;
-            }
-            ViewBag.KhoaCu = giangVien.MaKhoa;
+           
+           
             return View(giangVien);
         }
         [HttpPost]
         public ActionResult Suathongtin(GiangVien giangVien)
         {
+            QLSVEntities db = new QLSVEntities();
             db.Entry(giangVien).State = EntityState.Modified;
+            if (string.IsNullOrEmpty(giangVien.GvUser))
+            {
+                ViewBag.GvUserError = "Yêu cầu nhập tài khoản giảng viên.";
+            }
+            if (string.IsNullOrEmpty(giangVien.GvPass))
+            {
+                ViewBag.GvPassError = "Yêu cầu nhập mật khẩu giảng viên.";
+            }
+            if (string.IsNullOrEmpty(giangVien.HoTenGV))
+            {
+                ViewBag.HoTenGVError = "Yêu cầu nhập họ tên giảng viên.";
+            }
+
+            if (giangVien.NgaySinhGV == null || giangVien.NgaySinhGV == DateTime.MinValue)
+            {
+                ModelState.Remove("NgaySinhGV"); // Xóa ModelState Error cho trường này
+                ViewBag.NgaySinhGVError = "Yêu cầu nhập ngày sinh giảng viên.";
+            }
+
+
+            // Kiểm tra trường "NgaySinhGV" và đảm bảo năm sinh sau 2006
+            // Kiểm tra trường "NgaySinhGV" và đảm bảo năm sinh sau 2006
+            if (giangVien.NgaySinhGV.Year > 2006)
+            {
+                ModelState.AddModelError("NgaySinhGV", "Chưa đủ 18 tuổi.");
+            }
+
+            // Kiểm tra trường "GioiTinh"
+            // Kiểm tra trường "GioiTinh"
+            if (giangVien.GioiTinh == null)
+            {
+                ModelState.AddModelError("GioiTinh", "Yêu cầu chọn giới tính giảng viên.");
+            }
+
+
+
+            if (string.IsNullOrEmpty(giangVien.MaKhoa))
+            {
+                ViewBag.MaKhoaError = "Yêu cầu chọn mã khoa giảng viên.";
+            }
+
+            if (string.IsNullOrEmpty(giangVien.GvUser) || string.IsNullOrEmpty(giangVien.GvPass) || string.IsNullOrEmpty(giangVien.HoTenGV) ||
+                giangVien.NgaySinhGV == null || giangVien.GioiTinh == null || string.IsNullOrEmpty(giangVien.MaKhoa))
+            {
+                ViewBag.ErrorMessage = "Yêu cầu nhập đủ thông tin.";
+                return View(giangVien);
+            }
+
+            // Nếu dữ liệu hợp lệ, tiến hành thêm mới giảng viên
+            
+           
+
+
+
+
             db.SaveChanges();
             return RedirectToAction("DanhSachgiangVien");
         }
